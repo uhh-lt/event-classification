43e30b3ce5ba63eb007fea4334b5b36e8f1ffed0
Hans Ole Hatzel <hhatzel@gmail.com>
2021-06-06 12:25:37 +0200

    Third party parser integrations for spacy

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,8 @@ Preprocess raw documents to perform event classification on.
 from typing import Optional
 import os
 import json
+import bisect
+from typing import List
 
 import catma_gitlab as catma
 import typer
@@ -13,59 +15,74 @@ from thinc.api import set_gpu_allocator, require_gpu
 
 from event_classify.datasets import SimpleEventDataset
 import event_classify.segmentations # noqa: W0611
+from event_classify.parser import HermaParser, ParZuParser, Parser
 
 app = typer.Typer()
 
 
-def build_pipeline() -> spacy.Language:
-    nlp = spacy.load("de_dep_news_trf")
-    nlp.add_pipe("event_segmentation", after="parser")
+def build_pipeline(parser: Parser) -> spacy.Language:
+    if parser == Parser.SPACY:
+        nlp = spacy.load("de_dep_news_trf")
+        nlp.add_pipe("event_segmentation", after="parser")
+    elif parser == Parser.PARZU:
+        nlp = spacy.load("de_dep_news_trf", disable=["parser"])
+        nlp.add_pipe("sentencizer")
+        nlp.add_pipe("parzu_parser")
+        nlp.add_pipe("event_segmentation", after="parzu_parser")
+    elif parser == Parser.HERMA:
+        nlp = spacy.load("de_dep_news_trf", disable=["parser"])
+        nlp.add_pipe("sentencizer")
+        nlp.add_pipe("herma_parser")
+        nlp.add_pipe("event_segmentation", after="herma_parser")
     return nlp
 
 
 @app.command()
-def preprocess(text_file_path: str, out_file_path: str, title: Optional[str] = None, gpu: bool = False):
+def preprocess(text_file_paths: List[str], out_file_path: str, title: Optional[str] = None, gpu: bool = False, parser: Parser = Parser.SPACY):
     """
-    Segment document into event spans based on verb occurences.
+    Segment a set document into event spans based on verb occurrences.
 
-    Creates a JSON file with the document and its event spans.
+    Creates a JSON file with the document and its event spans suitable for passing to the `predict.py`.
     """
     if gpu:
         set_gpu_allocator("pytorch")
         require_gpu(0)
 
-    in_file = open(text_file_path, "r")
-    full_text = "\n".join(in_file.readlines())
-    nlp = build_pipeline()
-    doc = nlp(full_text)
-    if title is None:
-        title, _ = os.path.splitext(os.path.basename(out_file_path))
-    data = {
-        "text": full_text,
-        "title": title,
-        "annotations": []
-    }
-    for event_ranges in doc._.events:
-        spans = []
-        for subspan in event_ranges:
-            spans.append((subspan.start_char, subspan.end_char))
-        data["annotations"].append({
-            "start": min([start for start, end in spans]),
-            "end": max([end for start, end in spans]),
-            "spans": spans,
-            "predicted": None,
-        })
-    json.dump(data, open(out_file_path, "w"))
+    nlp = build_pipeline(parser)
+    document_list = []
+    for text_file_path in text_file_paths:
+        in_file = open(text_file_path, "r")
+        full_text = "\n".join(in_file.readlines())
+        doc = nlp(full_text)
+        if title is None:
+            title, _ = os.path.splitext(os.path.basename(out_file_path))
+        data = {
+            "text": full_text,
+            "title": title,
+            "annotations": []
+        }
+        for event_ranges in doc._.events:
+            spans = []
+            for subspan in event_ranges:
+                spans.append((subspan.start_char, subspan.end_char))
+            data["annotations"].append({
+                "start": min([start for start, end in spans]),
+                "end": max([end for start, end in spans]),
+                "spans": spans,
+                "predicted": None,
+            })
+        document_list.append(data)
+    json.dump(document_list, open(out_file_path, "w"))
 
 
 @app.command()
-def spans(input_sentence: str, display: bool = False):
-    nlp = build_pipeline()
+def spans(input_sentence: str, display: bool = False, parser: Parser = Parser.SPACY):
+    nlp = build_pipeline(parser)
     doc = nlp(input_sentence)
     if display:
         spacy.displacy.serve(doc, style="dep")
     for token in doc:
-        print(token, token.tag_, token.pos_, token.dep_)
+        print(token, token.tag_, token.pos_, token.dep_, token._.custom_dep)
     for ranges in doc._.events:
         print("======")
         for event_range in ranges:
@@ -73,11 +90,9 @@ def spans(input_sentence: str, display: bool = False):
 
 
 @app.command()
-def eval():
+def eval(parser: Parser = Parser.SPACY):
     """
-    Segment document into event spans based on verb occurences.
-
-    Creates a JSON file with the document and its event spans.
+    Evaluate segmentation outputs.
     """
     # set_gpu_allocator("pytorch")
     # require_gpu(0)
@@ -85,7 +100,7 @@ def eval():
     verwandlung_dataset, text = get_verwandlung()
     for annotation in verwandlung_dataset:
         gold_external_spans.add((annotation.start, annotation.end))
-    nlp = build_pipeline()
+    nlp = build_pipeline(parser)
     doc = nlp(text)
     predict_external_spans = set()
     for event_ranges in doc._.events:
@@ -96,17 +111,23 @@ def eval():
     accuracy =  overlap / max([len(predict_external_spans), len(gold_external_spans)])
     predict = sorted(predict_external_spans)
     gold = sorted(gold_external_spans)
-    for p, g in zip(predict[:40], gold[:40]):
-        print("======")
-        print(g, p)
+    for gold_span in gold[:100]:
+        print("===========")
+        pred_span = predict[find_best_match_for_start(predict, gold_span)]
         print("GOLD:")
-        print(text[g[0]:g[1]])
+        print(text[gold_span[0]:gold_span[1]])
         print("Predict:")
-        print(text[p[0]:p[1]])
+        print(text[pred_span[0]:pred_span[1]])
     print("Num Predicted:", len(predict_external_spans))
     print("Num Gold", len(gold_external_spans))
     print("Accuracy:", accuracy)
-    import ipdb; ipdb.set_trace()
+
+
+def find_best_match_for_start(sorted_list, element):
+    i = bisect.bisect_left(sorted_list, element)
+    if i != len(sorted_list):
+        return i
+    raise ValueError
 
 
 def get_verwandlung():
